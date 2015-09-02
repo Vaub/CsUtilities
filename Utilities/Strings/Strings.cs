dbf941ee25dbfa5431b26f70d31c4e1ae7e0a962
Vincent <vincent.aube@hotmail.com>
2015-09-01 23:52:48 -0400

    Join is in C#, good news!

--- Utilities/Strings/Strings.cs
+++ Utilities/Strings/Strings.cs
@@ -8,15 +8,9 @@ namespace Utilities.Strings
 {
     public static class Strings
     {
-        public static string Join(this char separator, IEnumerable<string> items)
+        public static string Join(this string separator, IEnumerable<string> items)
         {
-            StringBuilder builder = new StringBuilder();
-            foreach (string item in items)
-            {
-                builder.Append($"{item}{separator}");
-            }
-
-            return builder.ToString().TrimEnd(separator);
+            return string.Join(separator, items);
         }
     }
 }
